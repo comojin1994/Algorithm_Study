cae3264cd125e9855564077565702a74db606fbc
comojin1994 <comojin1994@gmail.com>
2020-02-12 18:51:12 +0900

    Add solutions of 15649

---
+++ Sungjin/BackTracking/n15649/15649_2.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+def func(index, N, M):
+    if index == M:
+        print(' '.join(map(str, result)))
+        return 1
+
+    for n in nums:
+        if check[n-1]: continue
+        check[n-1] = True
+        result[index] = n
+        func(index+1, N, M)
+        check[n-1] = False
+
+if __name__ == '__main__':
+    N, M = map(int, input().strip().split())
+    nums = [i+1 for i in range(N)]
+    check = [False for _ in range(N)]
+    result = [0 for _ in range(M)]
+    index = 0
+    func(index, N, M)
