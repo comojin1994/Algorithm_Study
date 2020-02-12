465116e091fd56abe8c61aa6c58ea90a91e63686
comojin1994 <comojin1994@gmail.com>
2020-02-13 03:02:36 +0900

    Add solution of 2443

---
+++ Sungjin/InOut/2443.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+def star():
+    for i in range(N-1, -1, -1):
+        print(' '*(N-i-1), end='')
+        print('*'*(2*i+1))
+    return 1
+
+if __name__ == '__main__':
+    N = int(input())
+    star()
