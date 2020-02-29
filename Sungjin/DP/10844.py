e3f65bfc8d66e55ddcc64b34b910fe96f0087778
comojin1994 <comojin1994@gmail.com>
2020-02-29 18:42:04 +0900

    Add solution of 10844

---
+++ Sungjin/DP/10844.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+if __name__ == '__main__':
+    N = int(input())
+    result = [[0, 1, 1, 1, 1, 1, 1, 1, 1, 1]] + [[0]*10 for _ in range(N-1)]
+
+    for i in range(1, N):
+        for j in range(10):
+            if j == 0: result[i][j] = result[i-1][j+1]
+            elif j == 9: result[i][j] = result[i-1][j-1]
+            else:
+                result[i][j] = result[i-1][j-1] + result[i-1][j+1]
+
+    print(sum(result[N-1]) % 10**9)
