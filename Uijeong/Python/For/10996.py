b3879aa85eeae965e9f17cde59f7b0ac44a322a0
Uijeong97 <uiui_97@naver.com>
2020-03-20 22:57:29 +0900

    For in Python

--- Uijeong/Python/For/10996.py
+++ Uijeong/Python/For/10996.py
@@ -1,9 +1,14 @@
 import sys
+import math
 input = sys.stdin.readline
 
 if __name__ == "__main__":
     n = int(input())
-    for i in range(2 * n):
-        print('*'*(i+1), end='')
-
-
+    for i in range(n):
+        for j in range(math.ceil(n/2)):
+            print('*', end=' ')
+        print()
+        print(' ', end='')
+        for j in range(n//2):
+            print('*', end=' ')
+        print()
