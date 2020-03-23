95bcd572b08eab04139a4fbe3c17f9de86498757
comojin1994 <comojin1994@gmail.com>
2020-03-23 12:45:18 +0900

    Add review code of 1904

---
+++ Sungjin/DP/n1904/1904_re.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+if __name__ == '__main__':
+    N = int(input())
+    dp = [0] * (N + 1)
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3, N+1):
+        dp[i] = ((dp[i-1] % 15746) + (dp[i-2] % 15746)) % 15746
+    print(dp[-1])
