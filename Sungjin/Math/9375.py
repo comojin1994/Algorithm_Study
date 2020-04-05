61c873c8cda9b27d9cfec91617896ffd9e9f3838
comojin1994 <comojin1994@gmail.com>
2020-04-05 13:57:35 +0900

    Part solution of 9375

--- Sungjin/Math/9375.py
+++ Sungjin/Math/9375.py
@@ -1,6 +1,20 @@
 import sys
-from itertools import combinations as cb
 input = sys.stdin.readline
+
+def func(idx, n, j, seq):
+    global result
+    if idx == n:
+        result += seq
+        return 1
+
+    for i in range(j, len(nums)):
+        if check[i]: continue
+
+        check[i] = True
+        seq *= nums[i]
+        func(idx + 1, n, i, seq)
+        check[i] = False
+        seq = seq//nums[i]
 
 if __name__ == '__main__':
     T = int(input())
@@ -11,3 +25,20 @@ if __name__ == '__main__':
             value, key = input().strip().split()
             if key in dict.keys(): dict[key] += 1
             else: dict[key] = 1
+        result = 0
+        nums = list(dict.values())
+        for i in range(1, len(dict.keys()) + 1):
+            check = [False] * len(nums)
+            seq = 1
+            func(0, i, 0, seq)
+        print(result)
+'''
+1
+6
+hat headgear
+sunglasses eyewear
+turban headgear
+mask face
+sunglasses face
+makeup face
+'''
