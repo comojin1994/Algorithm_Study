88f345cc92762ab1e368bdc82c08bbb268dc85a9
comojin1994 <comojin1994@gmail.com>
2020-04-06 12:13:20 +0900

    Add solution of 9375

--- Sungjin/Math/9375.py
+++ Sungjin/Math/9375.py
@@ -1,21 +1,6 @@
 import sys
 input = sys.stdin.readline
 
-def func(idx, n, j, seq):
-    global result
-    if idx == n:
-        result += seq
-        return 1
-
-    for i in range(j, len(nums)):
-        if check[i]: continue
-
-        check[i] = True
-        seq *= nums[i]
-        func(idx + 1, n, i, seq)
-        check[i] = False
-        seq = seq//nums[i]
-
 if __name__ == '__main__':
     T = int(input())
     for _ in range(T):
@@ -25,13 +10,10 @@ if __name__ == '__main__':
             value, key = input().strip().split()
             if key in dict.keys(): dict[key] += 1
             else: dict[key] = 1
-        result = 0
-        nums = list(dict.values())
-        for i in range(1, len(dict.keys()) + 1):
-            check = [False] * len(nums)
-            seq = 1
-            func(0, i, 0, seq)
-        print(result)
+        result = 1
+        for n in list(dict.values()):
+            result *= n+1
+        print(result-1)
 '''
 1
 6
