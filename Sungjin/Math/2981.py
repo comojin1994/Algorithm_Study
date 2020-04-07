143de254d87ce8730ec632c14b0774fb72dce794
comojin1994 <comojin1994@gmail.com>
2020-04-07 15:20:32 +0900

    Add new solution of 2981

--- Sungjin/Math/2981.py
+++ Sungjin/Math/2981.py
@@ -10,7 +10,7 @@ def gcd(n, m):
 
 def get_divisior(n):
 	result = set()
-	for i in range(1, (n//2) + 1):
+	for i in range(1, int(n**(0.5)) + 1):
 		if n % i == 0:
 			result.add(i)
 			result.add(n//i)
