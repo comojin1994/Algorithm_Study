1c6a3fc8776ce81220d1928e487e115823c32f36
SungJin Kim <sungjinkim@SungJinui-MacBookPro.local>
2020-04-07 15:12:33 +0900

    Add solution of 2004

---
+++ Sungjin/Math/2004.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+def get_cnt(n, num):
+    cnt = 0
+    i = 1
+    while num**i <= n:
+        cnt += n // num**i
+        i += 1
+    return cnt
+
+def cal(n ,m, num):
+    return get_cnt(n, num) - (get_cnt(m, num) + get_cnt(n-m, num))
+
+if __name__ == '__main__':
+    n, m = map(int, input().strip().split())
+    print(min(cal(n, m, 5), cal(n, m, 2)))
