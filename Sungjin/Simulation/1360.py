26a73ca35677e4b28c51cb433588afafddea2101
comojin1994 <comojin1994@gmail.com>
2020-04-16 22:33:09 +0900

    Add part solution of 1360

---
+++ Sungjin/Simulation/1360.py
@@ -0,0 +1,50 @@
+import sys
+input = sys.stdin.readline
+
+if __name__ == '__main__':
+    N = int(input())
+    Ord = [list(input().strip().split()) + [True] for _ in range(N)]
+    result = ''
+    for i in range(N - 1, -1, -1):
+        if Ord[i][0] == 'undo':
+            idx = int(Ord[i][2]) - int(Ord[i][1])
+            j = i - 1
+            while j >= 0:
+                if int(Ord[j][2]) < idx: break
+                if Ord[j][0] == 'undo':
+                    Ord[j][-1] = not Ord[j][-1]
+                j -= 1
+    for i in range(N - 1, -1, -1):
+        if Ord[i][0] == 'undo' and Ord[i][-1]:
+            idx = int(Ord[i][2]) - int(Ord[i][1])
+            j = i - 1
+            while j >= 0:
+                if int(Ord[j][2]) < idx: break
+                if Ord[j][0] == 'type':
+                    Ord[j][-1] = not Ord[j][-1]
+                j -= 1
+    for li in Ord:
+        if li[0] == 'type' and li[-1]: result += li[1]
+    print(result)
+
+'''
+4
+type a 1
+type b 2
+undo 2 3
+undo 2 4
+
+5
+type a 1
+type b 2
+undo 2 3
+undo 2 4
+undo 2 5
+
+5
+type a 1
+type b 2
+undo 2 3
+undo 2 4
+undo 2 6
+'''
