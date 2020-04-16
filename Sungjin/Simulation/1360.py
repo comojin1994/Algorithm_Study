b03a724b1b4d14eb211042fe45d7562781c65985
comojin1994 <comojin1994@gmail.com>
2020-04-16 22:49:35 +0900

    Add solution of 1360

--- Sungjin/Simulation/1360.py
+++ Sungjin/Simulation/1360.py
@@ -4,9 +4,10 @@ input = sys.stdin.readline
 if __name__ == '__main__':
     N = int(input())
     Ord = [list(input().strip().split()) + [True] for _ in range(N)]
+    # type a 1 True //  undo 3 5 True
     result = ''
     for i in range(N - 1, -1, -1):
-        if Ord[i][0] == 'undo':
+        if Ord[i][0] == 'undo' and Ord[i][-1]:
             idx = int(Ord[i][2]) - int(Ord[i][1])
             j = i - 1
             while j >= 0:
@@ -46,5 +47,13 @@ type a 1
 type b 2
 undo 2 3
 undo 2 4
+undo 2 6
+
+6
+type a 1
+type b 2
+undo 2 3
+undo 2 4
+type c 5
 undo 2 6
 '''
