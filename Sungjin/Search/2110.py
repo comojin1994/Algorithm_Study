1590d56159b3cf8b4a82bd4c3b0fa785632b4584
comojin1994 <comojin1994@gmail.com>
2020-04-22 11:12:41 +0900

    Add solution of 2110

--- Sungjin/Search/2110.py
+++ Sungjin/Search/2110.py
@@ -4,30 +4,36 @@ input = sys.stdin.readline
 def getMid(min_, max_):
     return (min_ + max_) // 2
 
-def checkCnt(n):
-    start = H[0]
-    cnt = 1
-    for h in H:
-        if h >= start + n:
+def checkCnt(mid):
+    start, cnt = 0, 1
+    for idx, h in enumerate(H):
+        if H[start] + mid <= h:
             cnt += 1
-            start = h
-            # print('h', h)
-    # print('cnt', cnt, 'n', n)
+            start = idx
     return cnt
 
 if __name__ == '__main__':
     N, C = map(int, input().strip().split())
     H = sorted([int(input()) for _ in range(N)])
     min_, max_ = 1, H[-1] - H[0]
+    result = 0
     while min_ <= max_:
         mid = getMid(min_, max_)
-        cnt = checkCnt(mid)
-        if mid - min_ <= 1 or max_ - mid <= 1: break
+        cnt= checkCnt(mid)
+        if cnt < C: max_ = mid - 1
+        else: min_ = mid + 1; result = mid
+    print(result)
 
-        if cnt > C:
-            min_ = mid - 1
-        elif cnt <= C:
-            max_ = mid + 1
-        # print(min_, mid, max_)
-    for n in range(max_, mid - 1, -1):
-        if checkCnt(n) >= C: print(n); break
+'''
+4 3
+999999985
+999999991
+999999996
+1000000000
+
+4 3
+1
+3
+5
+7
+'''
