3a4b24e844ff6e160554f9b8cc5cba5730b7ab7c
comojin1994 <comojin1994@gmail.com>
2020-04-24 13:17:34 +0900

    Add solution of 11286

---
+++ Sungjin/Deque/11286.py
@@ -0,0 +1,11 @@
+import sys, heapq
+input = sys.stdin.readline
+
+if __name__ == '__main__':
+    nums = []
+    for _ in range(int(input())):
+        n = int(input())
+        if n == 0:
+            if len(nums) == 0: print(0)
+            else: print(heapq.heappop(nums)[1])
+        else: heapq.heappush(nums, ([abs(n), n], n))
