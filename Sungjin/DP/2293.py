7653c10f15b3da90c0c0d0e611a1b64097be86e4
comojin1994 <comojin1994@gmail.com>
2020-04-26 00:56:13 +0900

    Add solution of 2293

--- Sungjin/DP/2293.py
+++ Sungjin/DP/2293.py
@@ -4,10 +4,9 @@ input = sys.stdin.readline
 if __name__ == '__main__':
     n, k = map(int, input().strip().split())
     val = [int(input()) for _ in range(n)]
-    dp = [[0] * (n + 1) for _ in range(k + 1)]
-    for idx, v in enumerate(val):
-        dp[v][idx] = 1
-    for i in range(1, k + 1):
-        for j in range(n-1, -1, -1):
-            dp[i][j] += dp[i - val[j]][j] + dp[i][j+1] if i - val[j] >= 0 else dp[i][j+1]
-    print(dp[-1][0])
+    dp = [0] * (k + 1)
+    dp[0] = 1
+    for i in range(n):
+        for j in range(val[i], k + 1):
+            dp[j] += dp[j - val[i]]
+    print(dp[k])
