912c268e3f357ec482157bdcb7b59e020d971881
comojin1994 <comojin1994@gmail.com>
2020-05-01 10:30:23 +0900

    Add solution of 1012

--- Sungjin/BFS/1012.py
+++ Sungjin/BFS/1012.py
@@ -6,9 +6,8 @@ def checkOne(i, j):
     L = [[i - 1, j], [i, j - 1], [i + 1, j], [i, j + 1]]
     result = []
     for li in L:
-        if li[0] < 0 or li[1] < 0 or li[0] > N - 1 or li[1] > N - 1: continue
-        if graph[li[1]][li[0]] == 1: result.append([li[0], li[1]])
-    # print('re', result)
+        if li[0] < 0 or li[1] < 0 or li[0] > N - 1 or li[1] > M - 1: continue
+        if graph[li[0]][li[1]] == 1: result.append([li[0], li[1]])
     return result
 
 def BFS(i, j):
@@ -17,20 +16,19 @@ def BFS(i, j):
     while que:
         n = que.popleft()
         li = checkOne(n[0], n[1])
-        graph[i][j] = 0
+        graph[n[0]][n[1]] = 0
         if n not in visited:
-            for ele in li:
-                if ele not in visited: que += [[ele[0], ele[1]]]
-                graph[ele[1]][ele[0]] = 0
-            visited.append([n])
-        # print('visit', visited)
+            for l in li:
+                if l not in visited: que += [l]
+            visited.append(n)
     return visited
 
 def main():
+    cnt = 0
     for i in range(N):
         for j in range(M):
-            if graph[i][j] == 1: print(BFS(i, j))
-    return 1
+            if graph[i][j] == 1: BFS(i, j); cnt += 1
+    return cnt
 
 if __name__ == '__main__':
     for _ in range(int(input())):
@@ -39,4 +37,16 @@ if __name__ == '__main__':
         for _ in range(K):
             x, y = map(int, input().strip().split())
             graph[y][x] = 1
-        main()
+        print(main())
+
+'''
+10 8 17
+[1, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
+[0, 0, 1, 1, 0, 0, 0, 1, 1, 1]
+[0, 0, 0, 0, 1, 0, 0, 1, 1, 1]
+[0, 0, 0, 0, 0, 0, 0, 1, 1, 1]
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+'''
