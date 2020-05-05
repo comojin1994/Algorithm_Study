6c23df71da9e881f186578540dc02195d8f01516
comojin1994 <comojin1994@gmail.com>
2020-05-05 18:27:10 +0900

    Add part solution of 7576

--- Sungjin/BFS/7576.py
+++ Sungjin/BFS/7576.py
@@ -1,6 +1,13 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
+
+def checkMinus(result):
+    if len(result) == 0: return True
+    cnt = 0
+    for r in result:
+        if graph[r[0]][r[1]] == -1: cnt += 1
+    if cnt == len(result): return False
+    return True
 
 def checkZero(i, j):
     L = [[i - 1, j], [i, j - 1], [i + 1, j], [i, j + 1]]
@@ -15,14 +22,17 @@ def BFS():
     for i in range(N):
         for j in range(M):
             if graph[i][j] == 1:
-                result += checkZero(i, j)
-    if len(result) == 0: return result, False
-    return result, True
+                temp = checkZero(i, j)
+                if not checkMinus(temp): return result, True, False
+                result += temp
+    if len(result) == 0: return result, False, True
+    return result, True, True
 
 def main():
     cnt = 0
     while True:
-        L, flag = BFS()
+        L, flag, flag2 = BFS()
+        if not flag2: cnt = -1; break
         if not flag: break
         for li in L:
             graph[li[0]][li[1]] = 1
