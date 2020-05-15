59986ae36899978b706380bd592df2dc1d696226
comojin1994 <comojin1994@gmail.com>
2020-05-15 21:42:39 +0900

    Add part solution of 13549

--- Sungjin/BFS/13549.py
+++ Sungjin/BFS/13549.py
@@ -5,24 +5,23 @@ input = sys.stdin.readline
 def BFS(N, K):
     que = deque([N])
     visited = [-1] * 100001
-    L = [N * 2 ** k for k in range(17) if N * 2 ** k < 100001]
-    for i in L:
-        visited[i] = 0
+    L = [N * (2 ** k) for k in range(17) if N * (2 ** k) < 100000]
+    for n in L:
+        visited[n] = 0
+
     while que:
         n = que.popleft()
         cnt = visited[n]
-        if n < 50001 and visited[2 * n] <= 0 :
+        if n < 50001 and (visited[2 * n] < 0 or visited[2 * n] > cnt):
+            visited[2 * n] = cnt
             que.append(2 * n)
-            if n not in L:
-                visited[2 * n] = cnt
-        if n < 100000 and (visited[n + 1] < 0 or visited[n + 1] > cnt + 1):
-            que.append(n + 1)
+        if n < 100000 and (visited[n + 1] < 0 or visited[n + 1] > cnt):
             visited[n + 1] = cnt + 1
-        if n > 0 and (visited[n - 1] < 0 or visited[n - 1] > cnt + 1):
-            que.append(n - 1)
+            que.append(n + 1)
+        if n > 0 and (visited[n - 1] < 0 or visited[n - 1] > cnt):
             visited[n - 1] = cnt + 1
-
-    print(visited[:19])
+            que.append(n - 1)
+    print(visited[:10])
     return visited[K]
 
 if __name__ == '__main__':
