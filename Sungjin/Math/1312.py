8a72217d70f6be3a9b2a9c5fdcb09349d3d173e1
comojin1994 <comojin1994@gmail.com>
2020-05-17 12:44:49 +0900

    Add solution of 1312

--- Sungjin/Math/1312.py
+++ Sungjin/Math/1312.py
@@ -3,16 +3,12 @@ input = sys.stdin.readline
 
 if __name__ == '__main__':
     A, B, N = map(int, input().strip().split())
-    if A < B: cnt = 0
-    elif A > B: cnt = -1
-    else: print(0); sys.exit(0)
+    cnt = 0
+    if A >= B: A = A % B
 
-    while cnt != N:
-        q, r = divmod(A, B)
-        if r == 0: q = 0; break
-        if q == 0:
-            A = r * 10
-        else:
-            A = r * 10
-            cnt += 1
-    print(q)
+    while cnt != N + 1:
+        Q = A // B
+        r = A % B
+        A = r * 10
+        cnt += 1
+    print(Q)
