9ea332f18403b3af0c910ff772bed7816f50fee8
comojin1994 <comojin1994@gmail.com>
2020-09-15 20:11:55 +0900

    Add part solution of 1339

--- Sungjin/Greedy/1339.py
+++ Sungjin/Greedy/1339.py
@@ -2,27 +2,50 @@ import sys
 input = sys.stdin.readline
 
 def solution():
-    num_li = {0:'0'}
+    num_li = {'0':'0'}
+    word_cnt = {'0':0}
     words = [list(input().strip()) for _ in range(int(input()))]
     words = sorted(words, key=lambda x: len(x), reverse=True)
+
     for idx, word in enumerate(words):
-        words[idx] = [0] * (8 - len(word)) + word
+        words[idx] = ['0'] * (8 - len(word)) + word
+
+    total = len(words)
+    for idx, word in enumerate(zip(*words)):
+        for w in word:
+            if w == '0': continue
+            if w not in word_cnt:
+                word_cnt[w] = 0
+            else:
+                word_cnt[w] += (total - idx)
 
     cnt = 9
-    for alpha in zip(*words):
-        for w in alpha:
-            if w == 0: continue
+    for word in zip(*words):
+        word = sorted(word, key=lambda x: word_cnt[x], reverse=True)
+        for w in word:
+            if w == '0': continue
             if w not in num_li.keys():
-                num_li[w] = str(cnt)
+                num_li[w] = cnt
                 cnt -= 1
 
-    result = 0
-    for i, word in enumerate(words):
-        for j, w in enumerate(word):
-            words[i][j] = num_li[w]
-        result += int(''.join(words[i]))
+    answer = 0
+    for word in words:
+        result = ''
+        for w in word:
+            result += str(num_li[w])
+        answer += int(result)
 
-    return result
+    return answer
 
 if __name__ == '__main__':
     print(solution())
+
+'''    
+6
+ABABABAB
+BABABABA
+ABABABAB
+BABABABA
+CCCCCCCA
+CCCCCCBA
+'''
